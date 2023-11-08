a4b4657a8e2f253a97d2fb33e5f0d0ac3000289b
root <ceccorac@gmail.com>
2023-11-08 21:55:09 +0000

    Project edited

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -42,7 +42,7 @@ def turn(speed, seconds):
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
 
-def find_token_color(v):
+def find_token(v):
     """
     Function to find the closest token
 
@@ -51,29 +51,33 @@ def find_token_color(v):
 	rot_y (float): angle between the robot and the token (-1 if no token is detected)
     """
     dist=500
-    mark = R.see()
     print("Marker already seen: " + str(v))
     for token in R.see():
-        if token.dist < dist and not (token.info.code in v): # conditions to verify if there are any token in the arena.
+    	code = token.info.code
+        if token.dist < dist and not (code in v): # conditions to verify if there are any token in the arena.
             dist=token.dist
 	    rot_y=token.rot_y
-    #if len(mark) == 0:
-    #	return find_token_color(v) # if the robot can'
     if dist==100:
-	return -1, -1
+	return -1, -1, -1
     else:
-   	return dist, rot_y
-   	
-   	
+   	return code, dist, rot_y
+    	
+
 def seeCenterArena():
+
+	'''
+
+	This function return the distance and the angle between the robot and the center of the arena
+
+	Returns:
+		dist_arena (float): distance of the center of the arena
+		rot_arena (float): angle between the robot and the arena '''
 	R.see()
         with R.lock:
             x, y = R.location
             heading = R.heading
 
-        acq_time = time.time()
-
-        rel_x, rel_y = (-x, -y)
+        rel_x, rel_y = (-x, -y)  
 
         dist_arena = hypot(rel_x, rel_y)
         rot_arena = degrees(atan2(rel_y, rel_x) - heading)
@@ -82,78 +86,75 @@ def seeCenterArena():
         
         
    	
-v = []
-dist_arena, rot_arena = seeCenterArena() #look for the center of arena.
+v = [] # this vector contain the code of the marker grabbed and released in the center of the arena
+dist_arena, rot_arena = seeCenterArena() # look for the center of arena.
+
 
 while 1:
 
     dist_arena = dist_arena + 0.5;
     
-    markers = R.see()
-    for m in markers:
-    	v = v
-    	
-    dist, rot_y = find_token_color(v)  # we look for markers.
     
-    if dist==-1:
-        print("I don't see any token!!")
-        turn(45, 0.5)
+    markers = R.see() 
+    
+    while not markers:
+    	dist_arena, rot_arena = seeCenterArena() # look for the center of the arena
+	turn(-25, 0.1)
+	markers = R.see()
+	for m in markers:
+		if m.info.code in v:
+			markers = []
+    	if abs(rot_arena) <= a_th:
+		exit()
+		
+    code, dist, rot_y = find_token(v)  # we look for markers. 	
 	
-    elif dist < d_th:
+    if dist < d_th:
     
     	print("Found it!")
     	
         grabbed = R.grab() # if we are close to the token, we grab it.
         
-        v.append(m.info.code) # put into the vector v the code of the marker grabbed.
+        v.append(code) # put into the vector v the code of the marker grabbed.
+        print(v)
         
         print("Gotcha!")
         
         while (d_arena < dist_arena or 0.4 < abs(rot_arena)) and grabbed is True:
         	
-        	dist_arena, rot_arena = seeCenterArena()
+        	dist_arena, rot_arena = seeCenterArena() # look for the center of the arena
 	    	
 	    	if rot_arena < -a_th:
 			
-			turn(-10, 0.25) # turn left if the robot can't see the center of arena
+			turn(-10, 0.1) # turn left if the robot can't see the center of arena
         	
 		elif rot_arena > a_th:
 			
-			turn(+10, 0.25) # turn right if the robot can't see the center of arena
+			turn(+10, 0.1) # turn right if the robot can't see the center of arena
 			
-		elif -a_th<= rot_arena <= a_th: # if the robot is well aligned with the token, we go forward
+		elif -a_th<= rot_arena <= a_th: 
 			
-			drive(50, 0.5)
+			drive(50, 0.2) # if the robot is well aligned with the token, we go forward
 			
 		if d_arena > dist_arena:
 		
-			grabbed = R.release() # release the marker when the robot is in the center of arena
+			R.release() # release the marker when the robot is in the center of arena
 			grabbed = False
-			print("Box released" + str(grabbed) )
-			
+						
 			drive(-50, 1)
 			
 			turn(-35, 1)
-			
-    
+			print (rot_arena)
+			    
     elif -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
         print("Ah, here we are!.")
-        drive(50, 0.5)
+        drive(50, 0.2)
         
     elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
         print("Left a bit...")
-        turn(-5, 0.25)
+        turn(-10, 0.1)
     
     elif rot_y > a_th:
         print("Right a bit...")
-        turn(+5, 0.25)
+        turn(+10, 0.1)
         
-    box = R.see() # control if there are any token in the arena
-			
-    if not box:
-	turn(15, 0.5)
-	if not box:
-		turn(400, 1)
-		if not box:
-			exit()
-
